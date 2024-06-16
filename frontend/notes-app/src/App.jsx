5add4a34aeaecd2d16c4ae4fc036c230ac1c8fc6
vimansha <vimanshamadushani88@gmail.com>
2024-06-16 12:18:24 +0530

    feat: tailwind and react-router-dom setup

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const routes = (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/signup" exact element={<SignUp />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <div>{routes}</div>;
 };
 
 export default App;

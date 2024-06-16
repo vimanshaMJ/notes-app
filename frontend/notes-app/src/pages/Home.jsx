34fc9fdc56ee92b9fd1e1f9de20266a3feca04af
vimansha <vimanshamadushani88@gmail.com>
2024-06-16 23:22:31 +0530

    feat: tags input component

--- frontend/notes-app/src/pages/Home.jsx
+++ frontend/notes-app/src/pages/Home.jsx
@@ -51,7 +51,13 @@ const Home = () => {
         contentLabel=""
         className="w-[50%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
       >
-        <AddEditNotes />
+        <AddEditNotes
+          type={openAddEditModal.type}
+          noteData={openAddEditModal.data}
+          onClose={() => {
+            setOpenAddEditModal({ isShown: false, type: "add", data: null });
+          }}
+        />
       </Modal>
     </div>
   );

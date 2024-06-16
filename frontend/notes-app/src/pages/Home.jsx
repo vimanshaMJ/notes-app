8a91c8e3879a02993e2279f553b248dcc2325062
vimansha <vimanshamadushani88@gmail.com>
2024-06-16 20:46:17 +0530

    feat: Note Card

--- frontend/notes-app/src/pages/Home.jsx
+++ frontend/notes-app/src/pages/Home.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import NoteCard from "../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Meeting on 7th April"
+            date="3rd April 2024"
+            content="Meeting on 7th April"
+            tags="#Meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {}}
+      >
+        <MdAdd className="text-[32px] text-white" />
+      </button>
     </div>
   );
 };

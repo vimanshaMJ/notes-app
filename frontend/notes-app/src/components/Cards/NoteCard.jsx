ec865340161ad92f13ca31c069fe47c4a5101aff
vimansha <vimanshamadushani88@gmail.com>
2024-06-16 22:01:10 +0530

    feat: Add/Edit note popup

--- frontend/notes-app/src/components/Cards/NoteCard.jsx
+++ frontend/notes-app/src/components/Cards/NoteCard.jsx
@@ -12,7 +12,7 @@ const NoteCard = ({
   onPinNote,
 }) => {
   return (
-    <div className="border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out">
+    <div className="border rounded p-4 bg-white hover:shadow-lg transition-all ease-in-out">
       <div className="flex items-center justify-between">
         <div>
           <h6 className="text-sm font-medium"> {title} </h6>
--- frontend/notes-app/src/pages/Home.jsx
+++ frontend/notes-app/src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import NoteCard from "../components/Cards/NoteCard";
 import { MdAdd } from "react-icons/md";
+import AddEditNotes from "./AddEditNotes";
+import Modal from "react-modal";
 
 const Home = () => {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
+
   return (
     <div>
       <Navbar />
@@ -25,10 +33,26 @@ const Home = () => {
 
       <button
         className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
-        onClick={() => {}}
+        onClick={() => {
+          setOpenAddEditModal({ isShown: true, type: "add", data: null });
+        }}
       >
         <MdAdd className="text-[32px] text-white" />
       </button>
+
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2)",
+          },
+        }}
+        contentLabel=""
+        className="w-[50%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+        <AddEditNotes />
+      </Modal>
     </div>
   );
 };

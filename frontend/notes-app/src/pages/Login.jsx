2668ee915ffa71c447441c7c030d2d69fea87f17
vimansha <vimanshamadushani88@gmail.com>
2024-06-16 15:23:35 +0530

    feat: login input components

--- frontend/notes-app/src/pages/Login.jsx
+++ frontend/notes-app/src/pages/Login.jsx
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 import PasswordInput from "../components/PasswordInput";
+import { validateEmail } from "../utils/helper";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address."); //if email is not valid
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError(""); //if email is valid
+
+    //Login API call
+  };
+
   return (
     <div>
       <Navbar />
 
       <div className="flex item-center justify-center mt-28">
         <div className="w-96 border rounded bg px-7 py-10">
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
-            <input type="text" placeholder="Email" className="input-box" />
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-            <PasswordInput />
+            <PasswordInput
+              value={password}
+              onchange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1"> {error} </p>}
 
             <button type="submit" className="btn-primary">
               Login

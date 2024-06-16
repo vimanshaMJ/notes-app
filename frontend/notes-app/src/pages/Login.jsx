2534634a76f00088c53ca916a20a2b1b7e138a7b
vimansha <vimanshamadushani88@gmail.com>
2024-06-16 14:03:32 +0530

    feat: login UI

--- frontend/notes-app/src/pages/Login.jsx
+++ frontend/notes-app/src/pages/Login.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+import PasswordInput from "../components/PasswordInput";
 
 const Login = () => {
   return (
     <div>
-      <p>Login</p>
+      <Navbar />
+
+      <div className="flex item-center justify-center mt-28">
+        <div className="w-96 border rounded bg px-7 py-10">
+          <form onSubmit={() => {}}>
+            <h4 className="text-2xl mb-7">Login</h4>
+            <input type="text" placeholder="Email" className="input-box" />
+
+            <PasswordInput />
+
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+            <p className="text-sm text-center mt-4">
+              Not registered yet?{" "}
+              <Link to="/signup" className="font-medium text-primary underline">
+                Create an account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/notes-app/tailwind.config.js
+++ frontend/notes-app/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Ensures all files in src are included
+    "./pages/**/*.{html,js,jsx}", // Include files in pages directory
+    "./components/**/*.{html,js,jsx}", // Include files in components directory
+  ],
   theme: {
-    extend: {},
+    extend: {
+      //colors user in the project
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E",
+      },
+    },
   },
   plugins: [],
 };

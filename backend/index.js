f1c4b86c17eaac001ec8cfddb9721f65e1014beb
vimansha <vimanshamadushani88@gmail.com>
2024-06-19 01:21:07 +0530

    feat: login API

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   res.json({ data: "hello" });
 });
 
-//create account
+//create account API
 app.post("/create-account", async (req, res) => {
   const { fullName, email, password } = req.body;
 
@@ -73,6 +73,43 @@ app.post("/create-account", async (req, res) => {
     accessToken,
     message: "Registration Successfull!",
   });
+});
+
+//login API
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: "Email is required" });
+  }
+
+  if (!password) {
+    return res.status(400).json({ message: "Password is required" });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "36000m",
+    });
+
+    return res.json({
+      error: false,
+      message: "Login Successfull!",
+      email,
+      accessToken,
+    });
+  } else {
+    return res
+      .status(400)
+      .json({ error: true, message: "Invalid credentials" });
+  }
 });
 
 app.listen(8000);

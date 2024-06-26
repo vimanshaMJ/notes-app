0be96dc32d4896a8ae8c2dfdd7245254a4f8e44d
vimansha <vimanshamadushani88@gmail.com>
2024-06-26 23:17:06 +0530

    feat: delete note API

--- backend/index.js
+++ backend/index.js
@@ -209,6 +209,32 @@ app.get("/get-all-notes", authenticateToken, async (req, res) => {
   }
 });
 
+//Delete note
+app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const { user } = req.user;
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+    if (!note) {
+      return res.status(404).json({ error: true, message: "Note not found" });
+    }
+
+    await note.deleteOne({ _id: noteId, userId: user._id });
+
+    return res.json({
+      error: false,
+      message: "Note deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal Server Error",
+    });
+  }
+});
+
 app.listen(8000);
 
 module.exports = app;
